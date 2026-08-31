efcc768707357c4023ffab02d7e12e4398c6cad4
aquaraga <4100682+aquaraga@users.noreply.github.com>
2026-09-01 04:54:54 +0530

    Grant content library access to server role

---
+++ supabase/migrations/20260901020000_grant_content_library_to_service_role.sql
@@ -0,0 +1,4 @@
+grant select on table public.courses to service_role;
+grant select on table public.chapters to service_role;
+grant select on table public.question_banks to service_role;
+grant select on table public.library_chapters to service_role;
